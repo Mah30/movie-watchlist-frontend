da8896103090896220a65af82669d9700105db06
Mariah <alinemariah@hotmail.com>
2026-06-29 11:10:35 +0200

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 
 /**
- * See https://playwright.dev/docs/test-configuration.
+ 
  */
 export default defineConfig({
   testDir: './tests',
@@ -26,7 +20,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-     baseURL: 'http://localhost:5173',
+     baseURL: 'http://127.0.0.1:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,15 +33,6 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-   /*  {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }, */
 
     /* Test against mobile viewports. */
     // {
@@ -71,11 +56,6 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 
       webServer: {
         command: "npm run dev -- --host 127.0.0.1",
