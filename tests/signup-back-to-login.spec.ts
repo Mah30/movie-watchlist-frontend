8f5be521e6eb7296e51d418d81d78d9aabcaf92b
Mariah <alinemariah@hotmail.com>
2026-07-06 11:11:10 +0200

    feat: add signup back to login test and run it

---
+++ tests/signup-back-to-login.spec.ts
@@ -0,0 +1,16 @@
+
+import { expect, test } from '@playwright/test';
+
+test( "gest can navigate from signup page back to login page", async({ page }) => {
+    await page.goto("/signup");
+    await expect(page).toHaveURL(/\/signup$/)
+
+
+    await expect(page.getByRole("heading", {name: "Sign Up"})).toBeVisible();
+    await expect(page.getByRole("button", {name: "Create Account"})).toBeVisible();
+
+    await expect(page.getByRole("link", {name: /already have an account\? log in/i })).toBeVisible()
+    await page.getByRole("link", {name: /already have an account\? log in/i }).click();
+
+
+})
